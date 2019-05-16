ea07df5cdd80b1a52dcb77766f4673db6e7acc0e
Ashna Shah-Grover <as19@williams.edu>
2019-05-16 20:00:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,6 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the_manifest = {"whale bone corsets"=>5, "porcelain vases"=>2,
   "oil_paintings"=>3}
-
 end
 
 def retrieval
